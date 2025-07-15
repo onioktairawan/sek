03642c0006eae516ae7ec1b6227acc08a6aab10b
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 14:41:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,16 +1,9 @@
 import asyncio
-import logging
 from telegram_handler import run_telegram_bot
 from discord_handler import run_discord_bot
 
-logging.basicConfig(
-    level=logging.INFO,
-    filename="bridge.log",
-    filemode="a",
-    format="%(asctime)s - %(levelname)s - %(message)s"
-)
-
 async def main():
+    print("[Main] Memulai bot...")
     await asyncio.gather(
         run_discord_bot(),
         run_telegram_bot()
