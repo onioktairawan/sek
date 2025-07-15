0f25cc29e7ac9251d1b4c7a5e85c820278ab6ce5
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 14:27:59 +0700

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,18 @@ import logging
 from telegram_handler import run_telegram_bot
 from discord_handler import run_discord_bot
 
-logging.basicConfig(level=logging.INFO, filename="bridge.log", filemode="a", format="%(asctime)s - %(levelname)s - %(message)s")
+logging.basicConfig(
+    level=logging.INFO,
+    filename="bridge.log",
+    filemode="a",
+    format="%(asctime)s - %(levelname)s - %(message)s"
+)
 
-if __name__ == '__main__':
-    loop = asyncio.get_event_loop()
-    loop.create_task(run_discord_bot())
-    loop.create_task(run_telegram_bot())
-    loop.run_forever()
+async def main():
+    await asyncio.gather(
+        run_discord_bot(),
+        run_telegram_bot()
+    )
+
+if __name__ == "__main__":
+    asyncio.run(main())
