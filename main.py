46eca48652b891fa8f16b81f9154dab0e2cafa2d
serpa <122547358+onioktairawan@users.noreply.github.com>
2025-07-15 15:51:12 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
 import asyncio
 from discord_handler import run_discord_bot
-from telegram_handler import run_telegram_bot
+from telegram_handler import run_telegram_bot, telegram_bot
 
 async def main():
     print("[Main] Memulai bot...")
 
-    tg_task = asyncio.create_task(run_telegram_bot())
+    # Jalankan Telegram dan tunggu selesai start
+    await run_telegram_bot()
 
-    await asyncio.sleep(2)  # Pastikan telegram_bot siap
-
-    dc_task = asyncio.create_task(run_discord_bot())
-
-    await asyncio.gather(tg_task, dc_task)
+    # Setelah telegram_bot siap, baru jalankan Discord
+    await run_discord_bot()
 
 if __name__ == "__main__":
     asyncio.run(main())
